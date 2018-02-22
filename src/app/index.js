17122ea82eef0f069c34b4102c3852deb4933291
Michal <m.star@wp.pl>
2018-02-22 15:20:27 +0100

    react-router

--- src/app/index.js
+++ src/app/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {Provider} from 'react-redux'
 
-import { store } from './state/store'
+import {store} from './state/store'
 import App from './views/App'
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <Route path='/' component={App} />
+    </Router>
   </Provider>,
   document.getElementById('root')
 )
