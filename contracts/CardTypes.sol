d1a4c643cb39ae9b6cdcdefab1d1c76581a1a595
Michal <m.star@wp.pl>
2018-02-27 21:45:31 +0100

    CardTypes contract

---
+++ contracts/CardTypes.sol
@@ -0,0 +1,42 @@
+pragma solidity ^0.4.0;
+
+import "./Ownable.sol";
+
+contract CardTypes is Ownable {
+
+    struct CardType {
+        uint dataId;
+        uint damage;
+        uint8 element;
+    }
+
+    struct Hero {
+        uint dataId;
+        uint health;
+    }
+
+    mapping (uint => CardType) cardTypes;
+    mapping (uint => Hero) heroes;
+    mapping (uint => bool) ishero;
+
+    function createCardType(uint id, uint dataId, uint damage, uint8 element) public onlyOwner {
+        cardTypes[id] = CardType(dataId, damage, element);
+        ishero[id] = false;
+    }
+
+    function createHero(uint id, uint dataId, uint health) public onlyOwner {
+        heroes[id] = Hero(dataId, health);
+        ishero[id] = true;
+    }
+
+    function getCardType(uint id) public view returns(uint, uint, uint8) {
+        CardType memory cardType = cardTypes[id];
+        return (cardType.dataId, cardType.damage, cardType.element);
+    }
+
+    function getHero(uint id) public view returns(uint, uint) {
+        Hero memory hero = heroes[id];
+        return (hero.dataId, hero.health);
+    }
+
+}
