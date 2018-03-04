f7dae74cb23fd05b245638fed20b167e93f426d3
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2018-03-04 19:35:07 +0100

    add shop page with a placeholder booster pack

--- src/app/views/shop/Shop.js
+++ src/app/views/shop/Shop.js
@@ -3,6 +3,8 @@ import React from 'react'
 const Shop = () =>
   <section className='container'>
     <h1>Shop</h1>
+    <img className='shop-booster' src='/images/placeholders/booster-pack.png' /><br />
+    <button>Open</button>
   </section>
 
 export default Shop
