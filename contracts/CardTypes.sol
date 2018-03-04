441ade84cfb8c4132f29a2f9dcb713f6571f1871
Michal <m.star@wp.pl>
2018-03-04 17:44:08 +0100

    refactor + cards test

--- contracts/CardTypes.sol
+++ contracts/CardTypes.sol
@@ -1,44 +1,44 @@
 pragma solidity ^0.4.0;
 
-import "./Ownable.sol";
+import "./lib/Ownable.sol";
 
 contract CardTypes is Ownable {
 
-    uint8 public elementAmount;
+  uint8 public elementAmount;
 
-    struct CardType {
-        uint dataId;
-        uint damage;
-        uint8 element;
-    }
+  struct CardType {
+    uint dataId;
+    uint damage;
+    uint8 element;
+  }
 
-    struct Hero {
-        uint dataId;
-        uint health;
-    }
+  struct Hero {
+    uint dataId;
+    uint health;
+  }
 
-    mapping (uint => CardType) cardTypes;
-    mapping (uint => Hero) heroes;
-    mapping (uint => bool) ishero;
+  mapping(uint => CardType) cardTypes;
+  mapping(uint => Hero) heroes;
+  mapping(uint => bool) ishero;
 
-    function createCardType(uint id, uint dataId, uint damage, uint8 element) public onlyOwner {
-        cardTypes[id] = CardType(dataId, damage, element);
-        ishero[id] = false;
-    }
+  function createCardType(uint id, uint dataId, uint damage, uint8 element) public onlyOwner {
+    cardTypes[id] = CardType(dataId, damage, element);
+    ishero[id] = false;
+  }
 
-    function createHero(uint id, uint dataId, uint health) public onlyOwner {
-        heroes[id] = Hero(dataId, health);
-        ishero[id] = true;
-    }
+  function createHero(uint id, uint dataId, uint health) public onlyOwner {
+    heroes[id] = Hero(dataId, health);
+    ishero[id] = true;
+  }
 
-    function getCardType(uint id) public view returns(uint, uint, uint8) {
-        CardType memory cardType = cardTypes[id];
-        return (cardType.dataId, cardType.damage, cardType.element);
-    }
+  function getCardType(uint id) public view returns (uint, uint, uint8) {
+    CardType memory cardType = cardTypes[id];
+    return (cardType.dataId, cardType.damage, cardType.element);
+  }
 
-    function getHero(uint id) public view returns(uint, uint) {
-        Hero memory hero = heroes[id];
-        return (hero.dataId, hero.health);
-    }
+  function getHero(uint id) public view returns (uint, uint) {
+    Hero memory hero = heroes[id];
+    return (hero.dataId, hero.health);
+  }
 
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,14 @@
 /* global artifacts */
 const Counter = artifacts.require('./Counter.sol')
 const CardStore = artifacts.require('./CardStore.sol')
+const Cards = artifacts.require('./Cards.sol')
+const Challenges = artifacts.require('./Challenges.sol')
+const CardTypes = artifacts.require('./CardTypes.sol')
 
 module.exports = function (deployer) {
   deployer.deploy(Counter)
   deployer.deploy(CardStore)
+  deployer.deploy(Cards)
+    .then(() => deployer.deploy(CardTypes)
+      .then(deployer.deploy(Challenges, Cards.address, CardTypes.address)))
 }
