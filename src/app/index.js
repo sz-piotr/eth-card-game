de5a05e863c01c43c4c6ca06e85945f6d3f125af
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2018-03-04 16:43:58 +0100

    make imports consistent

--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {BrowserRouter as Router} from 'react-router-dom'
-import {Provider} from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
-import {store} from './state/store'
+import { store } from './state/store'
 import App from './views/App'
 
 ReactDOM.render(
