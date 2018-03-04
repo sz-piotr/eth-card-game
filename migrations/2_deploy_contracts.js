fb9f8c82d8d96e46fd93b83d40540a433848920f
Michal <m.star@wp.pl>
2018-03-05 00:20:27 +0100

    cards test

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,11 +3,13 @@ const CardStore = artifacts.require('./CardStore.sol')
 const Cards = artifacts.require('./Cards.sol')
 const Challenges = artifacts.require('./Challenges.sol')
 const CardTypes = artifacts.require('./CardTypes.sol')
+const ERC721 = artifacts.require('./ERC721.sol')
 
 module.exports = function (deployer) {
   deployer.deploy(Counter)
   deployer.deploy(CardStore)
-  deployer.deploy(Cards)
-    .then(() => deployer.deploy(CardTypes)
-      .then(deployer.deploy(Challenges, Cards.address, CardTypes.address)))
+  deployer.deploy(ERC721)
+    .then(() => deployer.deploy(Cards))
+    .then(() => deployer.deploy(CardTypes))
+    .then(() => deployer.deploy(Challenges, Cards.address, CardTypes.address))
 }
--- test/cards.test.js
+++ test/cards.test.js
@@ -3,16 +3,16 @@ const Cards = artifacts.require('./Cards.sol')
 const number = 1
 const lvl = 1
 const metadata = 0
-const resultCard = {number: number, lvl: lvl, metadata: 1}
+const resultCard = [number, lvl, metadata]
 let cards
 
-contract('Cards', function () {
+contract('Cards', () => {
   it('should mint a card ', () => {
     Cards.deployed().then(instance => {
       cards = instance
-      return instance.owner()
-    }).then((address) => cards.mint(address, number, lvl, metadata))
-      .then(() => cards.cards(number))
-      .then(card => assert.equal(card, undefined))
+      return instance.minter.call()
+    }).then(cards.mint(adres, number, lvl, metadata))
+      .then(cards.cards.call(0))
+      .then(assert.equal(card.map(x => x.toNumber()).toString(), resultCard.toString()))
   })
 })
