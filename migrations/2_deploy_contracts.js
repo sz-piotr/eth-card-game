8e90c88aa5cc984a0108f5d47b27bf4728a5140b
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2018-03-06 14:21:59 +0100

    remove unused migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,16 +1,9 @@
 const Cards = artifacts.require('./Cards.sol')
 const Minter = artifacts.require('./Minter.sol')
 
-const Counter = artifacts.require('./Counter.sol')
-const CardStore = artifacts.require('./CardStore.sol')
-
 module.exports = function (deployer) {
   deployer.deploy(Cards)
     .then(() => deployer.deploy(Minter, Cards.address))
     .then(() => Cards.deployed())
     .then(cards => cards.setMinter(Minter.address))
-
-  // TODO: phase out
-  deployer.deploy(Counter)
-  deployer.deploy(CardStore)
 }
