23319a6598fa729a0d5ec5ffca430bfb677ad37c
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2018-04-07 19:53:04 +0200

    better pager and pagination styling

--- src/app/views/components/pagination/Pager.js
+++ src/app/views/components/pagination/Pager.js
@@ -3,14 +3,14 @@ import classnames from 'classnames'
 
 const VOID = 'javascript:void(0)'
 
-const FirstPagerItem = ({ onClick }) =>
-  <li className='pager-item pager-item--first'>
-    <a href={VOID} onClick={onClick} aria-label='First Page'>&laquo;</a>
+const PagerPrev = ({ onClick }) =>
+  <li className='pager-item pager-item--prev'>
+    <a href={VOID} onClick={onClick} aria-label='Previous'>&lt;</a>
   </li>
 
-const LastPagerItem = ({ onClick, disabled }) =>
-  <li className='pager-item pager-item--last'>
-    <a href={VOID} onClick={onClick} aria-label='Last Page'>&raquo;</a>
+const PagerNext = ({ onClick }) =>
+  <li className='pager-item pager-item--next'>
+    <a href={VOID} onClick={onClick} aria-label='Next'>&gt;</a>
   </li>
 
 const PagerItem = ({ children, active, onClick }) =>
@@ -18,7 +18,13 @@ const PagerItem = ({ children, active, onClick }) =>
     <a href={VOID} onClick={onClick}>{children}</a>
   </li>
 
+const PagerDots = () =>
+  <li className='pager-dots'>
+    ...
+  </li>
+
 const Pager = ({ page, count, onChange }) => {
+  // count = 40
   const pages = []
   for (let i = page - 2; i <= page + 2; i++) {
     if (i >= 0 && i < count) {
@@ -26,16 +32,28 @@ const Pager = ({ page, count, onChange }) => {
     }
   }
 
+  const showPrev = page !== 0
+  const showNext = page !== count - 1
+
+  const showFirst = pages[0] > 0
+  const showFirstDots = pages[0] > 1
+  const showLast = count > 1 && pages[pages.length - 1] < count - 1
+  const showLastDots = showLast && pages[pages.length - 1] < count - 2
+
   return (
     <nav className='pager' role='navigation'>
       <ul>
-        <FirstPagerItem onClick={() => onChange(0)} />
+        {showPrev && <PagerPrev onClick={() => onChange(page - 1)} />}
+        {showFirst && <PagerItem onClick={() => onChange(0)}>1</PagerItem>}
+        {showFirstDots && <PagerDots />}
         {pages.map(number =>
           <PagerItem key={number} active={number === page} onClick={() => onChange(number)}>
             {number + 1}
           </PagerItem>
         )}
-        <LastPagerItem onClick={() => onChange(count - 1)} />
+        {showLastDots && <PagerDots />}
+        {showLast && <PagerItem onClick={() => onChange(count - 1)}>{count}</PagerItem>}
+        {showNext && <PagerNext onClick={() => onChange(page + 1)} />}
       </ul>
     </nav>
   )
