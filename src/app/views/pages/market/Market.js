fae50cf19aa95997d0b893582b2538c117a16fe5
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2018-04-08 18:07:26 +0200

    mock market items

--- src/app/views/pages/market/Market.js
+++ src/app/views/pages/market/Market.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import CollectionFilter from '../collection/CollectionFilter'
+import Paginated from '../../components/pagination/Paginated'
+import MarketItem from './MarketItem'
+
+const mockData = new Array(400).fill(null)
 
 const Market = () =>
   <section className='container'>
     <h1>Market</h1>
+    <CollectionFilter /> {/* TODO: replace with market specific component */}
+    <ul className='market-items'>
+      <Paginated data={mockData} page={3} itemsPerPage={10} onChange={() => {}}>
+        {data => data.map((item, index) =>
+          <MarketItem key={index} />
+        )}
+      </Paginated>
+    </ul>
   </section>
 
 export default Market
---
+++ src/app/views/pages/market/MarketItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MarketItem = () =>
+  <li className='market-item'>
+    <img src='images/placeholders/card-icon.png' />
+    <div className='market-item-name'>
+      Annual Exterminator (Foil)
+    </div>
+  </li>
+
+export default MarketItem
