ca058ff697b91808d42709faa5015a7421e02681
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2018-04-14 11:31:52 +0200

    update truffle to newer version

--- truffle-config.js
+++ truffle-config.js
@@ -1,2 +1,6 @@
+const path = require('path')
+
 module.exports = {
+  working_directory: path.join(__dirname, 'src/ethereum'),
+  contracts_build_directory: path.join(__dirname, 'build/contracts')
 }
