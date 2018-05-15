9fd632c4485956a76776f22d4579bcba57a78ae0
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2018-05-15 23:41:37 +0200

    more sign transaction ui

--- src/frontend/app/views/components/info/SignTransaction.js
+++ src/frontend/app/views/components/info/SignTransaction.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
+import Icon from '../Icon'
+
 const SignTransaction = (props) =>
   <React.Fragment>
     <aside className='sign-transaction sign-transaction--open'>
       <div className='sign-transaction__title'>Sign Transaction</div>
+      <div className='alert'>
+        <Icon name='collection' />{' '}
+        Please review this before proceeding.
+      </div>
+      <p>Actions to be performed:</p>
+      <ul>
+        <li>Pay 0.01 ETH</li>
+        <li>Receive 3 random cards</li>
+      </ul>
+      <p>Executing the transaction requires <a className='link'>gas</a>.</p>
+      <button className='button button__wide'>Sign Transaction</button>
     </aside>
     <div className='sign-transaction__shadow' />
   </React.Fragment>
