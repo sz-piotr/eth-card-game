b3681ba960920efbd493a284e4ee89063e145838
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2018-05-15 23:02:25 +0200

    sign transaction aside

--- src/frontend/app/views/App.js
+++ src/frontend/app/views/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 
+import ErrorBoundary from './components/ErrorBoundary'
+
 import Navbar from './components/navigation/Navbar'
 import AlertBar from './components/info/AlertBar'
 import NotificationList from './components/info/NotificationList'
-import ErrorBoundary from './components/ErrorBoundary'
+import SignTransaction from './components/info/SignTransaction'
 
 import Arena from './pages/arena/Arena'
 import Collection from './pages/collection/Collection'
@@ -13,6 +15,7 @@ import Shop from './pages/shop/Shop'
 
 const App = () =>
   <ErrorBoundary>
+    <SignTransaction />
     <Navbar />
     <AlertBar />
     <NotificationList />
---
+++ src/frontend/app/views/components/info/SignTransaction.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignTransaction = (props) =>
+  <React.Fragment>
+    <aside className='sign-transaction sign-transaction--open'>
+      <div className='sign-transaction__title'>Sign Transaction</div>
+    </aside>
+    <div className='sign-transaction__shadow' />
+  </React.Fragment>
+
+export default SignTransaction
