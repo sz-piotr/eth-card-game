a2450f2beeefc3832dc405625c9c9651992a35d8
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2018-05-26 13:57:31 +0200

    add weiToEth util function

--- src/frontend/app/contracts/index.js
+++ src/frontend/app/contracts/index.js
@@ -1,6 +1,14 @@
+import * as Web3 from 'web3'
+
 export { Market } from './Market'
 export { Minter } from './Minter'
 export { Cards } from './Cards'
 export { Challenges } from './Challenges'
 
 export { waitForConfirmation } from './utils'
+
+const web3 = new Web3()
+
+export function weiToEth (wei) {
+  return web3.fromWei(wei, 'ether')
+}
