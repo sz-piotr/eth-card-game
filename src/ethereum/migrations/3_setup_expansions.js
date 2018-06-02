b24f121bd8ca2168b86a4c945b98c6e4fcf71164
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2018-06-02 15:25:41 +0200

    change migrations, force truffle tests, add safeTransferFrom to cards

--- src/ethereum/migrations/3_setup_expansions.js
+++ src/ethereum/migrations/3_setup_expansions.js
@@ -6,12 +6,5 @@ module.exports = function (deployer, network, accounts) {
 
 async function deploy (deployer, network, accounts) {
   const minter = await Minter.deployed()
-  await minter.createExpansion(3, 3, 2) // TODO: read from data/cards.json
-
-  for (let i = 0; i < 5; i++) {
-    await minter.purchasePack(0, {
-      from: accounts[0],
-      value: '10000000000000000' // 0.01 ETH
-    })
-  }
+  await minter.createExpansion(3, 3, 2) // from data/cards.json
 }
--- src/frontend/app/contracts/Cards.js
+++ src/frontend/app/contracts/Cards.js
@@ -1,11 +1,20 @@
 import { createContractInstance, createWeb3Function } from './utils'
 import { abi, networks } from '../../../../build/contracts/Cards'
 
-const instance = createContractInstance(abi, networks)
+// DIRTY HACK
+// Basically web3.js handles contract method overloading very poorly.
+// We remove an overloaded version of the safeTransferFrom function
+// in order to make sure the 4-argument version gets called.
+const dirtyHackedAbi = abi.filter(value =>
+  value.name !== 'safeTransferFrom' || value.inputs.length !== 3
+)
+
+const instance = createContractInstance(dirtyHackedAbi, networks)
 
 export const Cards = {
   // TODO: data immutable, use storage cache?
   getCard: createWeb3Function(instance, 'getCard'),
   ownerOf: createWeb3Function(instance, 'ownerOf'),
-  tokensOf: createWeb3Function(instance, 'tokensOf')
+  tokensOf: createWeb3Function(instance, 'tokensOf'),
+  safeTransferFrom: createWeb3Function(instance, 'safeTransferFrom')
 }
