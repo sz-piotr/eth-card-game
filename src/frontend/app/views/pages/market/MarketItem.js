95b5ff68b91fe26884098b5f1ce11a952a62a750
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2018-06-03 12:28:22 +0200

    clickable market items

--- src/frontend/app/views/pages/market/MarketItem.js
+++ src/frontend/app/views/pages/market/MarketItem.js
@@ -5,21 +5,21 @@ import { Link } from 'react-router-dom'
 import { getCardAttributes } from '../../../data'
 import { weiToEth } from '../../../contracts'
 
-const MarketItem = ({ details, seller, price }) => {
+const MarketItem = ({ cardId, details, seller, price }) => {
   const attributes = getCardAttributes(details.data)
   return (
     <li className='market-item'>
-      <img className='market-item-image' src='images/placeholders/square.svg' />
-      <div className='market-item-name'>
-        {attributes.displayName}
-      </div>
-      <Link to={`/collection/${seller}`} className='market-item-seller'>
-        {seller}
+      <Link to={`/collection/card/${cardId}`}>
+        <img className='market-item-image' src='images/placeholders/square.svg' />
+        <div className='market-item-name'>
+          {attributes.displayName}
+        </div>
+        <div className='market-item-seller'>{seller}</div>
+        <div className='market-item-price'>
+          <span className='market-item-value'>{weiToEth(price)}</span>
+          <span className='market-item-currency'>ETH</span>
+        </div>
       </Link>
-      <div className='market-item-price'>
-        <span className='market-item-value'>{weiToEth(price)}</span>
-        <span className='market-item-currency'>ETH</span>
-      </div>
     </li>
   )
 }
