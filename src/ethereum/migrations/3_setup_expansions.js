3c71c9032c5cf0c0a5775f9ee62ee0cadaea70e2
mstar95 <m.star@wp.pl>
2018-06-23 20:30:22 +0200

    deploy expansions

--- src/ethereum/migrations/3_setup_expansions.js
+++ src/ethereum/migrations/3_setup_expansions.js
@@ -1,18 +1,19 @@
 const Minter = artifacts.require('./Minter.sol')
 const CardTypes = artifacts.require('./CardTypes.sol')
 const migrateCards = require('../data/migrateCards.js')
+const cards = require('../data/cards.json')
 module.exports = function (deployer, network, accounts) {
   deployer.then(() => deploy(deployer, network, accounts))
 }
 
-async function deploy (deployer, network, accounts) {
+async function deploy(deployer, network, accounts) {
   const minter = await Minter.deployed()
   const cardTypes = await CardTypes.deployed()
-  await minter.createExpansion(2, 2, 1, 1, 1, 0) // from data/cards.json
+  deployExpansions(minter)
   migrateCards.migrateCards([deployCardTypes(cardTypes)])
 }
 
-function deployCardTypes (contract) {
+function deployCardTypes(contract) {
   return function (card) {
     if (card.type === 'hero') {
       contract.createHero(card.number, card.health)
@@ -20,4 +21,16 @@ function deployCardTypes (contract) {
       contract.createAction(card.number, card.damage, card.element)
     }
   }
+}
+
+function deployExpansions(contract) {
+  cards.expansions.forEach(expansion => {
+    const props = getExpansionsProps(expansion)
+    contract.createExpansion(...props)
+  })
+}
+
+function getExpansionsProps(expansion) {
+  return Object.entries(expansion.heroes).map(([_, heroes]) => heroes.length).concat(
+    Object.entries(expansion.actions).map(([_, actions]) => actions.length))
 }
