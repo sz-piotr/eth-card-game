7e44727164389d295e79f9af12374ddb7db7851c
Michal Starosta <michal.starosta@allegro.pl>
2018-06-24 17:38:33 +0200

    move arena data to file

--- src/frontend/app/views/pages/arena/ArenaSelection.js
+++ src/frontend/app/views/pages/arena/ArenaSelection.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import arenas from '../../../data/arenas.json'
 const ArenaOption = ({ id, image, name, description }) =>
   <li className='arena-option'><Link to={`/arena/locations/${id}`}>
     <img className='arena-option-image' src={image} />
@@ -15,30 +15,10 @@ const ArenaSelection = () =>
     <h2 className='page-title-secondary'>Start a new battle</h2>
     <nav>
       <ul className='arena-select'>
-        <ArenaOption id={1} image='images/arenas/urog-proving-grounds.png'
-          name='Urog Proving Grounds'
-          description='Level 1, common only'
-        />
-        <ArenaOption id={2} image='images/arenas/lone-moon.png'
-          name='Lone Moon'
-          description='Levels 1-2'
-        />
-        <ArenaOption id={3} image='images/arenas/drowned-world.png'
-          name='Drowned World'
-          description='Levels 2-3'
-        />
-        <ArenaOption id={4} image='images/arenas/ringworld.png'
-          name='Ringworld'
-          description='Levels 2-4'
-        />
-        <ArenaOption id={5} image='images/arenas/purgatory.png'
-          name='Purgatory'
-          description='Levels 3+'
-        />
-        <ArenaOption id={6} image='images/arenas/black-hole.png'
-          name='GX0874C - Singularity'
-          description='Levels 4+, rare and epic only'
-        />
+        {arenas.map((arena, id) => <ArenaOption id={id} image={arena.image}
+          name={arena.name}
+          description={arena.description}
+        />)}
       </ul>
     </nav>
   </section>
