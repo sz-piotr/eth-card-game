a681b8469bb2d4444db3f4a3e1f113f1c370e736
mstar95 <m.star@wp.pl>
2018-07-01 20:11:35 +0200

    arena location view

--- src/frontend/app/views/pages/arena/ArenaLocation.js
+++ src/frontend/app/views/pages/arena/ArenaLocation.js
@@ -2,18 +2,34 @@ import React from 'react'
 import Page from '../../components/Page'
 
 import arenas from '../../../data/arenas.json'
+import CardDisplay from '../../components/cards/CardDisplay';
+import Icon from '../../components/Icon';
 
 class ArenaLocation extends React.Component {
-  render() {
+  render () {
     const id = this.props.match.params.id
     const arena = arenas[id]
     return (
       < Page title={arena.name} >
-        <img className='arena-option-image' src={arena.image} />
+        <div className='arena-location'>
+          <img className='arena-location-image' src={arena.image} />
+          <button className='arena-new-challenge'>
+            <Icon name='arena' />
+          </button>
+          <div className='arena-location-opponents'>
+            <div className="arena-location-opponents-collection">
+              <Icon name='chevron-left' className='arena-location-icon' />
+              <CardDisplay data={["1100", "1", "0"]} />
+              <CardDisplay data={["1100", "1", "0"]} />
+              <CardDisplay data={["1100", "1", "0"]} />
+              <Icon name='chevron-right' className='arena-location-icon' />
+            </div>
+          </div>
+        </div>
+        {arena.wideDescription}
       </Page >
     )
   }
 }
-
 
 export default ArenaLocation
